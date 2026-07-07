4b9a745e9eb9c193dcafa5c296c5aa83dbfbaf75
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-07-07 14:48:19 +0800

    feat(route): daftarkan resource route books dan borrows

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// apiResource: sekali daftar langsung membuat 5 route CRUD (index, store, show, update, destroy)
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('books', BookController::class);
+Route::apiResource('borrows', BorrowController::class);
