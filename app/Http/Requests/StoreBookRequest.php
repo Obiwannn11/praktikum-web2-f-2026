7eacdfacc2cbc29be9677793031a5f1d52052d53
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-07-07 14:47:57 +0800

    fix(request): perbaiki field validasi StoreBookRequest

---
+++ app/Http/Requests/StoreBookRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            // exists: pastikan category_id benar-benar ada di tabel categories
+            'category_id' => 'required|integer|exists:categories,id',
+            'title' => 'required|string|max:255',
+            'writer' => 'required|string|max:255',
+            'release_date' => 'required|date',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'category_id.required' => 'Kategori Wajib Ada',
+            'category_id.exists' => 'Kategori Tidak Ditemukan',
+            'title.required' => 'Judul Wajib Diisi',
+            'title.max' => 'Maksimal Panjang Judul :max karakter',
+            'writer.required' => 'Penulis Wajib Diisi',
+            'writer.max' => 'Maksimal Panjang Penulis :max karakter',
+            'release_date.required' => 'Tanggal Terbit Wajib Diisi',
+            'release_date.date' => 'Format Tanggal Terbit Tidak Valid',
+        ];
+    }
+}
